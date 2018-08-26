1084b96fdf72e26aebf161b979769e0c584b2067
Andrew Kail <andrew.a.kail@gmail.com>
2018-08-26 16:54:49 -0400

    Generate Pelican site

--- theme/js/disqus.js
+++ theme/js/disqus.js
@@ -9,7 +9,7 @@ this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your pag
 */
 (function() { // DON'T EDIT BELOW THIS LINE
 var d = document, s = d.createElement('script');
-s.src = 'https://kail-io-1.disqus.com/embed.js';
+s.src = 'http://kail-io-1.disqus.com/embed.js';
 s.setAttribute('data-timestamp', +new Date());
 (d.head || d.body).appendChild(s);
 })();
